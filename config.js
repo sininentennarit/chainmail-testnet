6f6d25a7e6834c9be57d56357e04cd68d4bc0d46
Sininen Tennarit <sininentennarit@gmail.com>
2026-01-01 22:12:28 +0100

    Version 0.1.0

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+  cspr_click_app_name: "Test",
+  cspr_click_app_id: "5e9c7683-7d4b-4329-8560-1987a143",
+  cspr_click_providers: ['casper-wallet', 'ledger', 'metamask-snap'],
+  cspr_live_url: "https://testnet.cspr.live",
+
+  send_message_entry_point_id: 2680459,
+  chainmail_contract_package_hash: '25ab87b79553294557d4ad49774fce50c27ebdcf892c358aac11c51e75cc5b97',
+  message_token_contract_package_hash: "270d3324ccf10324f4661f0d9e036e9b31a207eaf1853b1a64ebffc3b294549e",
+  stamp_token_contract_package_hash: "881eb6b99809ed58d65d30e6b8823a189aa95eec47d71784869a9f92fd855999",
+  stamp_cost: 5,
+  stamp_transaction_cost: 9.5,
+  message_transaction_cost: 8,
+};
