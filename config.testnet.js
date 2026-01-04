188ece9a1889bb7bb7538c5cd17a7f22792368a6
Sininen Tennarit <sininentennarit@gmail.com>
2026-01-04 13:18:16 +0100

    Added Google Analytics

---
+++ config.testnet.js
@@ -0,0 +1,14 @@
+const config = {
+  cspr_click_app_name: "Test",
+  cspr_click_app_id: "5e9c7683-7d4b-4329-8560-1987a143",
+  cspr_click_providers: ['casper-wallet', 'ledger', 'metamask-snap'],
+  cspr_live_url: "https://testnet.cspr.live",
+
+  send_message_entry_point_id: 2681921,
+  chainmail_contract_package_hash: '8058e3c16943bd9eadefbd2b4d4fb1e32ce118e492e179c42e14ed3f71d5601d',
+  message_token_contract_package_hash: "01f8cb209aed9d94198fa71e339ab19d44f90b018bd7e7dbe474c56b4ecbbb15",
+  stamp_token_contract_package_hash: "b85da037a04930a02c5f4d333c5e5ea43a9b51619e5879e56323819887f63f8e",
+  stamp_cost: 5,
+  stamp_transaction_cost: 9.5,
+  message_transaction_cost: 8,
+};
