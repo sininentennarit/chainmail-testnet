82724e5baf47291489d637a9ccc9d90f537d1410
Sininen Tennarit <sininentennarit@gmail.com>
2026-03-31 19:15:22 +0200

    Added CSPR.click social login support

--- config.mainnet.js
+++ config.mainnet.js
@@ -1,10 +1,10 @@
 const config = {
   cspr_click_app_name: "ChainMail",
   cspr_click_app_id: "b4ec1b73-dcb1-4658-9c30-a741196d",
-  cspr_click_providers: ['casper-wallet', 'ledger', 'metamask-snap'],
+  cspr_click_providers: ['casper-wallet', 'ledger', 'metamask-snap', 'csprclick-w3a-google', 'csprclick-w3a-apple'],
   cspr_live_url: "https://cspr.live",
 
-  send_message_entry_point_id: 480192,
+  send_message_entry_point_id: 480280,
   chainmail_contract_package_hash: "55339d8be4335fcd28830d6e93a5addf3764cacf46fa2973850b9eda35e9e063",
   message_token_contract_package_hash: "f03fe4e6a37edeb02a4b5458961058a80a439b9cb54a0e3c12d620a4f445d78f",
   stamp_token_contract_package_hash: "c0148bac98dbdba400e5711c42b7b1601477f6ad06de30d1ca8d34b083af1074",
